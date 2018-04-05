f5009b4a6d4cb15de3880bf038418ef138eeaff5
jinyao <jinyao383@hotmail.com>
2018-04-05 21:37:40 +0200

    initial commit

---
+++ lightweight_filtering/cmake/FindLWF.cmake
@@ -0,0 +1,9 @@
+# This file was automatically generated during the installation of the lightweight_filtering library
+# and can be used through cmake to find the corresponding header files. A copy of this
+# file was created in /usr/share/cmake-3.5/Modules (depending on the CMAKE_ROOT variable).
+
+set(LWF_INCLUDE_DIRS
+/home/jinyao/ros_ws/rovio/install/include/LWF/include
+)
+set(LWF_FOUND TRUE)
+message("-- LWF found (include: /home/jinyao/ros_ws/rovio/install/include/LWF/include)")
